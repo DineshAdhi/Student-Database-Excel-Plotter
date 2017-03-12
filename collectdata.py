c3ea876041e78f569ad980e58d8b981366576e13
Dinesh <dinsesh10c04@gmail.com>
2017-03-12 19:53:52 +0530

    Initial commit

---
+++ collectdata.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+import urllib2
+import time
+import json
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from bs4 import BeautifulSoup
+import dill as pickle
+main_list = [];
+
+for i in range(02,50):
+        try:
+            browser = webdriver.PhantomJS('./phantomjs');
+            browser.get("http://125.17.181.197/StudentView.php");
+            if(i <= 9):
+                browser.find_element_by_name('reg_no').send_keys('31231410700'+str(i));
+            else:
+                browser.find_element_by_name('reg_no').send_keys('3123141070'+str(i));
+            browser.find_element_by_id('submit').click()
+            element_present = ec.presence_of_element_located((By.CLASS_NAME, 'black_overlay1'))
+            WebDriverWait(browser, 10).until(element_present)
+
+            soup = BeautifulSoup(browser.page_source.encode('utf-8'), 'html.parser')
+
+            cgpas = soup.find_all('table')[1].find_all('tr')[3].find_all('td')
+
+            list={};
+            list['reg_no']='3123141070'+str(i);
+            list['name']= soup.find_all('table')[0].find_all('tr')[2].find_all('td')[1].get_text()
+            for idx, i in enumerate(cgpas):
+                if(idx==9):
+                    list["total_cgpa"] = i.get_text();
+                    continue;
+                if(i.get_text()=="GPA/CGPA"):
+                    continue;
+                else:
+                    list["sem-"+str(idx)] = i.get_text()
+
+            main_list.append(list);
+            print main_list
+            browser.close()
+
+        except:
+            print("There was an error with rool no : " + str(i));
+
+
+
+with open('list.pkl', 'w') as src:
+    pickle.dump(main_list, src)
---
+++ excel_plotter.py
@@ -0,0 +1,43 @@
+import dill as pickle
+import json
+import xlwt
+
+with open('main_list.pkl','r') as src:
+    mylist = pickle.load(src);
+
+print json.dumps(mylist, indent=4, sort_keys=True)
+
+
+for i in mylist:
+    print i
+
+    
+
+
+book = xlwt.Workbook(encoding='utf-8')
+
+sheet = book.add_sheet('Sheet 1');
+
+sheet.write(0,0, "Register number");
+sheet.write(0,1, "Name");
+
+for i in range(1,9):
+    sheet.write(0, i+1, "Sem "+str(i));
+
+sheet.write(0, 10, "Total CGPA");
+
+
+for index, i in enumerate(mylist):
+    sheet.write(index+1, 0, i['reg_no'])
+    sheet.write(index+1, 1, i['name'])
+    sheet.write(index+1, 2, i['sem-1'])
+    sheet.write(index+1, 3, i['sem-2'])
+    sheet.write(index+1, 4, i['sem-3'])
+    sheet.write(index+1, 5, i['sem-4'])
+    sheet.write(index+1, 6, i['sem-5'])
+    sheet.write(index+1, 7, i['sem-6'])
+    sheet.write(index+1, 8, i['sem-7'])
+    sheet.write(index+1, 9, i['sem-8'])
+    sheet.write(index+1, 10, i['total_cgpa'])
+
+book.save('trials.xls')
